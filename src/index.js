8dd386473381656679c7a46274157101cdc2485d
Selin Serin <selin.serin@frontiercargroup.com>
2021-02-02 15:38:20 +0100

    fix merge conflict

--- src/index.js
+++ src/index.js
@@ -1,34 +1,60 @@
 const WORD_LIST = require("../data/wordList");
-  const LETTERS = require("../data/letters");
+const LETTERS = require("../data/letters");
 
-  const DEFAULT_BLOCK = 3;
-  const DEFAULT_NUM_OF_BLOCK_ELEMENTS = 3;
-  const DEFAULT_SEPARATOR = "-";
+const DEFAULT_BLOCK = 3;
+const DEFAULT_NUM_OF_BLOCK_ELEMENTS = 3;
+const DEFAULT_SEPARATOR = "-";
 
-  const generateLetterId = (block, numberOfEl, separator) =>
-    Array.from({ length: block || DEFAULT_BLOCK }).reduce(
-      (id, el, i) =>
-        id +
-        getBlock(numberOfEl || DEFAULT_NUM_OF_BLOCK_ELEMENTS) +
-        (i < block - 1 ? separator || DEFAULT_SEPARATOR : ""),
-      ""
-    );
+const generateLetterId = () => generateCustomLetterId();
 
-  const generateWordId = (block, separator, wordList) =>
-    Array.from({ length: block || DEFAULT_BLOCK }).reduce(
-      (id, el, id) =>
-        id +
-        (wordList || WORD_LIST)[getIndex((wordList || WORD_LIST).length - 1)] +
-        (i < block - 1 ? separator || DEFAULT_SEPARATOR : ""),
-      ""
-    );
+const generateCustomLetterId = (block=DEFAULT_BLOCK, numberOfEl=DEFAULT_NUM_OF_BLOCK_ELEMENTS, separator=DEFAULT_SEPARATOR)=>
+  Array.from({ length: block }).reduce(
+    (id, el, i) =>
+      id + getBlock(numberOfEl)+ (i < block - 1 ? separator : ''),
+    ''
+  );
 
-  const getIndex = (max) => Math.floor(Math.random() * max);
 
-  const getBlock = (numberOfEl) =>
-    Array.from({ length: numberOfEl }).reduce(
-      (block, i) => block + LETTERS[getIndex(LETTERS.length - 1)],
-      ""
-    );
+const getIndex = (max = LETTERS.length - 1) => Math.floor(Math.random() * max);
 
-  module.exports = { generateLetterId, generateWordId };
+const getBlock = (numberOfEl) =>
+  Array.from({ length: numberOfEl }).reduce(
+    (block, i) => block + LETTERS[getIndex()],
+    ""
+  );
+
+const generateWordId = (
+  block = DEFAULT_BLOCK,
+  separator = DEFAULT_SEPARATOR,
+  wordList = WORD_LIST
+) =>
+  Array.from({
+    length: block
+  }).reduce(
+    (id, el, i) =>
+      id +
+      wordList[getIndex(wordList.length - 1)] +
+      (i < block - 1 ? separator : ""),
+    ""
+  );
+
+  const generateIdStartingWith = (
+  startWith = '',
+  block = 1,
+  separator = DEFAULT_SEPARATOR,
+  numberOfEl=DEFAULT_NUM_OF_BLOCK_ELEMENTS
+) =>
+  Array.from({
+    length: block
+  }).reduce(
+    (id, el, i) =>
+      id + separator + getBlock(numberOfEl),
+    startWith
+  );
+
+module.exports = {
+  generateLetterId,
+  generateCustomLetterId,
+  generateWordId,
+  generateIdStartingWith
+};
